d827c5025a231fead64bdce14c88b128245390f5
Szilvasi Peter <peti.szilvasi95@gmail.com>
2023-11-16 16:15:08 +0100

    Create API to generate log folders

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import date, timedelta
 from pathlib import Path
 
 from fastapi import FastAPI
@@ -31,3 +32,39 @@ def generate_log(num_logs: int):
     for i in range(num_logs):
         logging.info(f"Generate logs {i}")
     return {"Log generation is done."}
+
+
+@app.get("/generate_log_folders/{num_folders}")
+def generate_log_folders(num_folders: int):
+    logger = logging.getLogger()
+
+    previous_handlers = logger.handlers.copy()
+    logger.handlers.clear()
+
+    create_file_handlers(num_folders, logger)
+    for i in range(5000):
+        logger.info(f"Hello, world {i}!")
+
+    logger.handlers.clear()
+    logger.handlers = previous_handlers
+
+    return {"Log folder generation is done."}
+
+
+def create_file_handlers(num_folders: int, logger: logging.Logger):
+    """Create logging file handlers that log inside different folders
+
+    Args:
+        num_folders (int): Number of folders to create
+        logger (Logger): Logger instance to which logging handlers are added
+    """
+    parent_log = Path("logs")
+    child_path = parent_log
+    today = date.today()
+    for i in range(num_folders):
+        child_path = parent_log / str(today + timedelta(days=i))
+        child_path.mkdir(parents=True, exist_ok=True)
+
+        filename = str(child_path / "my.log")
+        file_handler = logging.FileHandler(filename)
+        logger.addHandler(file_handler)
